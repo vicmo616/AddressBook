2a626a1d62a0f5aeff82483ea5c7d49bd6b9a805
Victor M. Ortiz <vicortiz1993@gmail.com>
2022-03-15 20:29:54 -0400

    Initial commit
    Established Connection to postgres pgadmin database via connection string in app settings after installing the necessary nuget packages, being entity frameworks and tools then updating the database through the package manager console to create the DBs container. proceeding to establish the inital model for the contact class that consisted of the necessary properties that the contact is contrived of, adding in also a migration so the DB knows that  i want to create the next iteration of the Db updating DB to reflect the new contacts table that was created as a new property in the application database class of the type contact class that possesses the properties that make up the columns that it is made up of and shown on pgadmin.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using AddressBook.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# AddressBook
+ An MVC Address Book That Communicates To A PgAdmin Database 

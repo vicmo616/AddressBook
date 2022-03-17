e858ffcfe98c92485608117e0ea3c84367c5b471
Victor M. Ortiz <vicortiz1993@gmail.com>
2022-03-16 23:52:45 -0400

    Added data utility class
    Added data utility class to the data folder holding two methods called GetConnectionSring and BuildConnectionString to be responsible for choosing which connection string to use to connect to the database. if running locally it will use local connection string, if on heroku, it will chop and form the database url environment variable into a viable ConnectionString.

---
+++ Data/DataUtility.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook.Data
+{
+    public static class DataUtility
+    {
+        public static string GetConnectionString(IConfiguration configuration)
+        {
+            //The default connection string will come from appsettings like usual
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            //it willl be automatically overwritten if we are running on heroku
+            var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+            return string.IsNullOrEmpty(databaseUrl) ? connectionString : BuildConnectionString(databaseUrl);
+        }
+        public static string BuildConnectionString(string databaseUrl)
+        {
+            //provides an object representation of a uniform resource identifier (URI) and easy
+            var databaseUri = new Uri(databaseUrl);
+            var userInfo = databaseUri.UserInfo.Split(':');
+
+            //provides a simple way to create and manage the contents of connection strings used
+            var builder = new NpgsqlConnectionStringBuilder
+            {
+                Host = databaseUri.Host,
+                Port = databaseUri.Port,
+                Username = userInfo[0],
+                Password = userInfo[1],
+                Database = databaseUri.LocalPath.TrimStart('/'),
+                SslMode = SslMode.Prefer,
+                TrustServerCertificate = true
+            };
+            return builder.ToString();
+        }
+           
+    }
+}
